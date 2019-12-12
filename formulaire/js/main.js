32c50a554b17f6bf4978980da7c4d9fc2bbc7736
quentin-lct62 <57142326+quentin-lct62@users.noreply.github.com>
2019-12-12 14:10:11 +0100

    ajout

---
+++ formulaire/js/main.js
@@ -0,0 +1,25 @@
+$("#buttonSend").on("click", function(event,target){
+checkEmptyInputs();
+});
+
+function checkEmptyInputs(){
+  $(".notempty").each(function (i , elt){
+    if(elt.value == ""){
+      $(elt).toggleClass("border-danger");
+      $(elt).next().toggleClass("d-none");
+      retour = false;
+    }
+    let  bp;
+  });
+  return retour;
+}
+
+
+function checkMailFormat(email){
+
+}
+
+
+function checkPasswords(){
+
+}
